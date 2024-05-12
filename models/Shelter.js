9de6dbbbce9bb4c55673ac7a65247b15ea506b66
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2024-05-12 17:45:02 +0530

    model for shelter

---
+++ models/Shelter.js
@@ -0,0 +1,45 @@
+// models/Shelter.js
+
+const mongoose = require('mongoose');
+
+const shelterSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    capacity: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    contact: {
+        email: {
+            type: String,
+            required: true
+        },
+        phone: {
+            type: String,
+            required: true
+        }
+    },
+    availability: {
+        type: Boolean,
+        default: true
+    },
+    photos: [
+        {
+            type: String,
+            required: true
+        }
+    ]
+}, { timestamps: true });
+
+const Shelter = mongoose.model('Shelter', shelterSchema);
+ export default Shelter
