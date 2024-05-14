b03925e238977d71932b636a00551436275bb11f
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2024-05-14 19:51:46 +0530

    complete signout logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -60,3 +60,13 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const signOut = (req, res, next) => {
+  try {
+      res.clearCookie('access_token')
+          .status(200)
+          .json('User has been signed out');
+  } catch (error) {
+      next(error);
+  }
+};
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getUserById, updateUser } from '../controllers/userController.js';
+import { getUserById, signOut, updateUser } from '../controllers/userController.js';
 import { verifyToken } from '../utils/authUtils.js';
 
 const router = express.Router();
 
 router.get('/get/:id', getUserById);
 router.put('/update/:userId',verifyToken,updateUser)
+router.post('/signout',signOut)
 
 export default router;
