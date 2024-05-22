9ae0b4a9fc99ab095bc3a4b27c7b4afad270ed5c
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2024-05-22 23:58:54 +0530

    remove otp logic

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,16 +1,9 @@
 import express from "express";
-import {
-  register,
-  login,
-  requestOtp,
-  verifyOtp,
-} from "../controllers/authController.js";
+import { register, login } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.post("/request-otp", requestOtp);
-router.post("/verify-otp", verifyOtp);
 
 export default router;
