87ccaf845e890baf50c60aa2e1adbd464a96615e
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2024-06-15 07:26:40 +0530

    Code fix

--- routes/responderRoute.js
+++ routes/responderRoute.js
@@ -7,11 +7,12 @@ import {
   deleteResponder,
 } from "../controllers/responderController.js";
 
+import { isVolunteer } from "./../utils/isVolunteer.js";
 const router = express.Router();
 
 router.get("/:id", getResponderById);
 router.put("/:id", updateResponder);
 router.delete("/:id", deleteResponder);
-router.post("/", createResponder);
+router.post("/", isVolunteer, createResponder);
 router.get("/", getAllResponders);
 export default router;
